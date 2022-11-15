5d54201e69b55fd4abc3ea7beeeaff1633f00ff2
Tadas Kepalas <tadas.kepalas@monetha.io>
2022-11-15 14:35:53 +0200

    Mirror specific branch

--- .github/actions/mirror_repo/scripts/mirror.sh
+++ .github/actions/mirror_repo/scripts/mirror.sh
@@ -4,4 +4,9 @@ set -eu
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no"
 git config --global --add safe.directory '*'
 git remote add mirror "$INPUT_TARGET_REPO_URL"
-git push --tags --prune --force mirror "refs/remotes/origin/*:refs/heads/*"
+if [ -z "$INPUT_SPECIFIC_BRANCH" ]
+then
+    git push --tags --prune --force mirror "refs/remotes/origin/*:refs/heads/*"
+else
+    git push --tags --prune --force mirror "refs/remotes/origin/$INPUT_SPECIFIC_BRANCH:refs/heads/$INPUT_SPECIFIC_BRANCH"
+fi
